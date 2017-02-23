c6fc7d93416faf9e2fa7c8e431f573cc837f4a05
ndmanvar <ndmanvar@yahoo.com>
2017-02-22 17:07:36 -0800

    adding in lots of rdc tests

--- Rakefile
+++ Rakefile
@@ -1,29 +1,8 @@
 def run_tests(deviceName, deviceType, platformName, platformVersion, app)
-  system("deviceName=\"#{deviceName}\" deviceType=#{deviceType} platformName=\"#{platformName}\" platformVersion=\"#{platformVersion}\" app=\"#{app}\" parallel_split_test spec")
+  system("deviceName=\"#{deviceName}\" deviceType=#{deviceType} platformName=\"#{platformName}\" platformVersion=\"#{platformVersion}\" app=\"#{app}\" PARALLEL_SPLIT_TEST_PROCESSES=30 parallel_split_test spec")
 end
 
-task :Andoid_Emulator_Phone_5_1 do
-  run_tests('Android Emulator', 'Phone', 'Android', '5.1', 'https://github.com/saucelabs-sample-test-frameworks/Java-Junit-Appium-Android/blob/master/resources/GuineaPigApp-debug.apk?raw=true')
+task :Galaxy_S6_Device do
+  run_tests('Samsung Galaxy S6 Device', '', 'Android', '6.0', 'https://github.com/saucelabs-sample-test-frameworks/Java-Junit-Appium-Android/blob/master/resources/GuineaPigApp-debug.apk?raw=true')
 end
 
-task :Andoid_Emulator_Tablet_5_1 do
-  run_tests('Android Emulator', 'Tablet', 'Android', '5.1', 'https://github.com/saucelabs-sample-test-frameworks/Java-Junit-Appium-Android/blob/master/resources/GuineaPigApp-debug.apk?raw=true')
-end
-
-task :Galaxy_S4_Emulator do
-  run_tests('Samsung Galaxy S4 Emulator', '', 'Android', '4.4', 'https://github.com/saucelabs-sample-test-frameworks/Java-Junit-Appium-Android/blob/master/resources/GuineaPigApp-debug.apk?raw=true')
-end
-
-task :Galaxy_S5_Device do
-  run_tests('Samsung Galaxy S5 Device', '', 'Android', '4.4', 'https://github.com/saucelabs-sample-test-frameworks/Java-Junit-Appium-Android/blob/master/resources/GuineaPigApp-debug.apk?raw=true')
-end
-
-multitask :test_sauce => [
-    :Andoid_Emulator_Phone_5_1,
-    :Galaxy_S4_Emulator,
-    :Andoid_Emulator_Tablet_5_1,
-    :Galaxy_S5_Device,
-
-  ] do
-    puts 'Running automation'
-end
--- spec/test1_spec.rb
+++ spec/test1_spec.rb
@@ -3,15 +3,336 @@ require_relative "spec_helper"
 describe "Guinea Pig" do
   it "enter a comment" do
     app = GuineaPigAppPage.new @driver
+    for i in 1..10 do
+      sleep 20
+      driver.page_source
+    end
     app.textInput.send_keys("My Exceptionally Eloquent Comment")
     expect(app.textInput.text == "My Exceptionally Eloquent Comment")
-    end
-
-  it "should enter an email address" do
-    app = GuineaPigAppPage.new @driver
-    app.emailTextInput.send_keys("example@email.com")
-    expect(app.emailTextInput.text == "example@email.com")
   end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email addres 1" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email addres 1" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
+  # it "should enter an email address" do
+  #   app = GuineaPigAppPage.new @driver
+  #   for i in 1..10 do
+  #     sleep 20
+  #     driver.page_source
+  #   end
+  #   app.emailTextInput.send_keys("example@email.com")
+  #   expect(app.emailTextInput.text == "example@email.com")
+  # end
+
 end
 
 
